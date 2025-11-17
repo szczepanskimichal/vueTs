0b0450a97ca18aba7353c20077a2101cb4a093c9
Michal Szczepanski <127307935+szczepanskimichal@users.noreply.github.com>
2025-11-17 22:10:10 +0100

    Restore missing directories and files for Nuxt 3 starter, Tailwind, layouts, components, and pages

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Navbar from "~/components/layout/navbar.vue";
+</script>
+<template>
+  <div class="h-screen">
+    <Navbar/>
+      <slot/>
+  </div >
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './components/**/*.{vue,js,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './app.vue',
+    './nuxt.config.{js,ts}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
